8409ed8cad36307668f4e4497f6c52d55bddb8b8
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-10-28 06:11:26 +0500

    Project01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Countdown from "@/components/count-down";
-=======
-import Countdown from "../../components/count-down";
->>>>>>> 345a39885da910219304b40f04bc9aaed3a8e2dd
 
 export default function Home() {
   return (
